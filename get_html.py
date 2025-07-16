5e457ed5824d7d3ff3b32b087e307fe0a7f9db7b
yamato-snow <yamato2016baseball@icloud.com>
2025-07-16 21:21:17 +0900

    HTMLスクレイピング用のURLを変更し、QiitaからTech-Campのコンテンツを取得するように修正。新たにLINE構築コースとWebデザインコースに関する詳細な記事を追加。

--- get_html.py
+++ get_html.py
@@ -21,10 +21,11 @@ def fetch_and_convert(url):
     # content = soup.find('div', id='mainContent') # incdesign.jp
     # content = soup.find('div', class_='article-main')
     # content = soup.find('div', id='content') # weel.co.jp
-    content = soup.find('div', class_='p-items_main') # qiita.com
+    # content = soup.find('div', class_='p-items_main') # qiita.com
     # content = soup.find('main', class_='cnt_main--v2') # sbbit.jp
     # content = soup.find('div', id='colmunLeft') # dreamnews.jp
-
+    content = soup.find('main') # tech-camp.in
+    
     if content:
         # HTMLをMarkdownに変換
         markdown = md(str(content))
@@ -37,5 +38,5 @@ def fetch_and_convert(url):
 
 if __name__ == "__main__":
     # 処理を開始するURL
-    url = 'https://qiita.com/tichise/items/6c4a21d47dc7ee0968eb'
+    url = 'https://www.tokyofreelance.jp/achievements/i-interview/'
     fetch_and_convert(url)
